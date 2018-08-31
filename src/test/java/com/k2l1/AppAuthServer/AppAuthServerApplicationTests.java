51ee99bf78b14b11cbe70b732bff5f135312ef85
hs-glenn-lee <git_test@mail.com>
2018-08-31 21:30:02 +0900

    First Commit

---
+++ src/test/java/com/k2l1/AppAuthServer/AppAuthServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.k2l1.AppAuthServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppAuthServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
