e1fab86361eb5443f3f6db8e79392bc0d7c283fb
hs-glenn-lee <git_test@mail.com>
2018-10-18 21:33:01 +0900

    little

--- src/main/java/com/k2l1/CreatedDocsServer/amqp/api/RabbitMqHttpApi.java
+++ src/main/java/com/k2l1/CreatedDocsServer/amqp/api/RabbitMqHttpApi.java
@@ -6,7 +6,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface RabbitMqHttpApi {
-
+	//http://host:port/api/queues/vhost/name
 	@GET("/api/queues/{vhost}/{qname}")
 	public Call<ResponseBody> getQueue(@Path("vhost")String vhost,	@Path("qname")String qname);
 
